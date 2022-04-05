0b8fb7cf821c6a8c31703a342d943f30d1c9f1b5
Matheus Carvalho <69327395+MatheusCarvalho95@users.noreply.github.com>
2022-04-04 22:33:30 -0300

    return with a comma instead of dot

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ import pandas
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello_values():
+   #df = pandas.read_excel('./app/ipca-t.xlsx','Sheet1' ).tail(12)
    df = pandas.read_excel('./app/ipca.xlsx','Sheet1').tail(12)
+   values_column = df['valor'].astype(float)
    total = 0
-   values_column = df['valor']
-   
    for i in list(values_column):
       total += i
    
-   return str(format(total, '.2f'))
+   return format(total, '.2f').replace('.', ',')
